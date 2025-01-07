5ccbf0e18b19a6fef55e4b25b7538daa8ad20272
ariima0 <c3004002@hallam.shu.ac.uk>
2025-01-07 09:12:37 +0000

    still fixin

--- styles/main.js
+++ styles/main.js
@@ -1,19 +1,19 @@
 const fadeContainer = document.querySelector('.image-fade-container');
 const images = fadeContainer.querySelectorAll('.fade-image');
-let currentIndex = 0; 
+let currentIndex = 0;
 
 function fadeImages() {
-    images[currentIndex].style.opacity = 0; 
-    currentIndex = (currentIndex + 1) % images.length; 
-    images[currentIndex].style.opacity = 1; 
+    images[currentIndex].style.opacity = 0; // Hide current image
+    currentIndex = (currentIndex + 1) % images.length; // Move to the next image
+    images[currentIndex].style.opacity = 1; // Show next image
 }
 
 let interval;
 
 fadeContainer.addEventListener('mouseover', () => {
-    interval = setInterval(fadeImages, 2000); 
+    interval = setInterval(fadeImages, 2000); // Start fading on hover
 });
 
 fadeContainer.addEventListener('mouseout', () => {
-    clearInterval(interval);
+    clearInterval(interval); // Stop fading on hover out
 });
