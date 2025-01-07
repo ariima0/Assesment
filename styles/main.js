edb45ca199de0a50b54564adfb0237e0643e945e
ariima0 <c3004002@hallam.shu.ac.uk>
2025-01-07 09:08:56 +0000

    broken

--- styles/main.js
+++ styles/main.js
@@ -1,19 +1,19 @@
 const fadeContainer = document.querySelector('.image-fade-container');
 const images = fadeContainer.querySelectorAll('.fade-image');
-let currentIndex = 0;
+let currentIndex = 0; 
 
 function fadeImages() {
-    images[currentIndex].style.opacity = 0; // Hide the current image
-    currentIndex = (currentIndex + 1) % images.length; // Move to the next image
-    images[currentIndex].style.opacity = 1; // Show the next image
+    images[currentIndex].style.opacity = 0; 
+    currentIndex = (currentIndex + 1) % images.length; 
+    images[currentIndex].style.opacity = 1; 
 }
 
 let interval;
 
 fadeContainer.addEventListener('mouseover', () => {
-    interval = setInterval(fadeImages, 2000); // Start fading on hover
+    interval = setInterval(fadeImages, 2000); 
 });
 
 fadeContainer.addEventListener('mouseout', () => {
-    clearInterval(interval); // Stop fading when hover ends
+    clearInterval(interval);
 });
